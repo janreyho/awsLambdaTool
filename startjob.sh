c91ff4a138ea810b7ca8953e557e6ed718f59e29
hejiayi <janreyho@gmail.com>
2016-03-10 10:56:33 +0800

    多个CP任务并行处理

--- startjob.sh
+++ startjob.sh
@@ -1,10 +1,17 @@
 var=` TZ='Asia/Shanghai' date +%Y-%m-%d `
+userfolder=~
 echo $var
 
 for file in ` ls ~/gochina/gochina/cpconfig `
 do
-	#~/gochina/gochina/gochinajob.sh no >> ~/gochina/$file/log/$var 2>&1
+{
+	ps -fe | grep "$userfolder/gochina/gochina/gochinajob.sh $1 $file" | grep -v grep 
+	if [ $? -ne 0 ]; then
 	~/gochina/gochina/gochinajob.sh $1 $file >> ~/gochina/$file/log/$var 2>&1
 	/usr/bin/mail -s $file'-'$var'更新' janreyho@gmail.com < ~/gochina/$file/log/$var
+	else
+		echo "$userfolder/gochina/gochina/gochinajob.sh $1 $file exist"
+	fi
+} &
 done
 
