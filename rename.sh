af1f8f9c76b080d15441f28cd54fabc4977acc17
hejiayi <janreyho@gmail.com>
2016-03-21 15:05:29 +0800

    /->/

--- rename.sh
+++ rename.sh
@@ -5,37 +5,37 @@ echo "First Method: ${txbmapsrc[*]}"
 echo "Second Method: ${txbmapdst[@]}"
 
 function fun(){
-        # cp $txbtool"/"txbvar.json $txbtool"/"$2.json
+        # cp $txbtool/txbvar.json $txbtool/$2.json
         echo {} > $txbtool/$2.json
-        jq --arg cpvar $2 '.cp=$cpvar' $txbtool"/"$2.json > $txbtool"/"var1.json
-        mv $txbtool"/"var1.json $txbtool"/"$2.json
-        jq --arg var $thumbgap '.thumbgap=$var' $txbtool"/"$2.json > $txbtool"/"var1.json
-        mv $txbtool"/"var1.json $txbtool"/"$2.json
+        jq --arg cpvar $2 '.cp=$cpvar' $txbtool/$2.json > $txbtool/var1.json
+        mv $txbtool/var1.json $txbtool/$2.json
+        jq --arg var $thumbgap '.thumbgap=$var' $txbtool/$2.json > $txbtool/var1.json
+        mv $txbtool/var1.json $txbtool/$2.json
         # 日期目录
         for file1 in ` ls $1 `
         do
-        if [ -d $1"/"$file1 ]; then
+        if [ -d $1/$file1 ]; then
                 echo $file1
                 pos1=0
                 # 专辑目录
-                for file2 in ` ls $1"/"$file1 `
+                for file2 in ` ls $1/$file1 `
                 do
-                        if [ -d $1"/"$file1"/"$file2 ]; then
+                        if [ -d $1/$file1/$file2 ]; then
                         pos2=0
                         # 节目名称
-                        for file3 in ` ls $1"/"$file1"/"$file2 `
+                        for file3 in ` ls $1/$file1/$file2 `
                         do
                         echo $file3
                           namevar=`echo $file3 | sed 's/\([0-9]*\)\(.*\)\(\..*\)/\2/g'`
                           idvar=`echo $file3 | sed 's/\([0-9]*\)\(.*\)\(\..*\)/\1/g'`
-                          jq --arg var $namevar --arg num1 $pos1 --arg num2 $pos2 '.albums[$num1 | tonumber].videos[$num2 | tonumber].name=$var' $txbtool"/"$2.json > $txbtool"/"var1.json
-                          mv $txbtool"/"var1.json $txbtool"/"$2.json
-                          jq --arg var $idvar --arg num1 $pos1 --arg num2 $pos2 '.albums[$num1 | tonumber].videos[$num2 | tonumber].id=$var' $txbtool"/"$2.json > $txbtool"/"var1.json
-                          mv $txbtool"/"var1.json $txbtool"/"$2.json
+                          jq --arg var $namevar --arg num1 $pos1 --arg num2 $pos2 '.albums[$num1 | tonumber].videos[$num2 | tonumber].name=$var' $txbtool/$2.json > $txbtool/var1.json
+                          mv $txbtool/var1.json $txbtool/$2.json
+                          jq --arg var $idvar --arg num1 $pos1 --arg num2 $pos2 '.albums[$num1 | tonumber].videos[$num2 | tonumber].id=$var' $txbtool/$2.json > $txbtool/var1.json
+                          mv $txbtool/var1.json $txbtool/$2.json
                           pos2=`expr $pos2 + 1`
                         done
 
-                        cd $1"/"$file1"/"$file2
+                        cd $1/$file1/$file2
                         rename 's/^(\d+)(?:[^.]*)(\.\w+)/$1$2/' *
                         #cd -
 
@@ -44,11 +44,11 @@ function fun(){
                                 echo $file2 | grep -q "${txbmapsrc[i]}"
                                 if [ $? -eq 0 ]; then
                                         echo $file2"->"${txbmapdst[i]}
-                                        jq --arg namevar ${txbmapsrc[i]} --arg num1 $pos1 '.albums[$num1 | tonumber].name=$namevar' $txbtool"/"$2.json > $txbtool"/"var1.json
-                                        mv $txbtool"/"var1.json $txbtool"/"$2.json
-                                        jq --arg idvar ${txbmapdst[i]} --arg num1 $pos1 '.albums[$num1 | tonumber].id=$idvar' $txbtool"/"$2.json > $txbtool"/"var1.json
-                                        mv $txbtool"/"var1.json $txbtool"/"$2.json
-                                        mv $1"/"$file1"/"$file2 $1"/"$file1"/"${txbmapdst[i]}
+                                        jq --arg namevar ${txbmapsrc[i]} --arg num1 $pos1 '.albums[$num1 | tonumber].name=$namevar' $txbtool/$2.json > $txbtool/var1.json
+                                        mv $txbtool/var1.json $txbtool/$2.json
+                                        jq --arg idvar ${txbmapdst[i]} --arg num1 $pos1 '.albums[$num1 | tonumber].id=$idvar' $txbtool/$2.json > $txbtool/var1.json
+                                        mv $txbtool/var1.json $txbtool/$2.json
+                                        mv $1/$file1/$file2 $1/$file1/${txbmapdst[i]}
                                 fi
                         done
                         fi
@@ -57,25 +57,25 @@ function fun(){
 
                 pos1=0
                 # 专辑目录
-                for file2 in ` ls $1"/"$file1 `
+                for file2 in ` ls $1/$file1 `
                 do
-                  if [ -d $1"/"$file1"/"$file2 ]; then
+                  if [ -d $1/$file1/$file2 ]; then
                   pos2=0
                   # 节目名称
-                  for file3 in ` ls $1"/"$file1"/"$file2 `
+                  for file3 in ` ls $1/$file1/$file2 `
                   do
-                          id=`jq --arg num1 $pos1 --arg num2 $pos2 '.albums[$num1 | tonumber].videos[$num2 | tonumber].id | tonumber' $txbtool"/"$2.json`
-                          jq --arg var $bucksrc"/"$file1"/"$file2"/"$id".mp4" --arg num1 $pos1 --arg num2 $pos2 '.albums[$num1 | tonumber].videos[$num2 | tonumber].src=$var' $txbtool"/"$2.json > $txbtool"/"var1.json
-                          mv $txbtool"/"var1.json $txbtool"/"$2.json
-                          jq --arg var $buckdst"/"$file2"/"$id"/"$id".m3u8" --arg num1 $pos1 --arg num2 $pos2 '.albums[$num1 | tonumber].videos[$num2 | tonumber].dst=$var' $txbtool"/"$2.json > $txbtool"/"var1.json
-                          mv $txbtool"/"var1.json $txbtool"/"$2.json
-                          jq --arg var $buckdst"/"$file2"/"$id"/00001.jpg" --arg num1 $pos1 --arg num2 $pos2 '.albums[$num1 | tonumber].videos[$num2 | tonumber].thumbnails=$var' $txbtool"/"$2.json > $txbtool"/"var1.json
-                          mv $txbtool"/"var1.json $txbtool"/"$2.json
-                          jq --arg var $buckdst"/"$file2"/"$id".png" --arg num1 $pos1 --arg num2 $pos2 '.albums[$num1 | tonumber].videos[$num2 | tonumber].pagepic=$var' $txbtool"/"$2.json > $txbtool"/"var1.json
-                          mv $txbtool"/"var1.json $txbtool"/"$2.json
+                          id=`jq --arg num1 $pos1 --arg num2 $pos2 '.albums[$num1 | tonumber].videos[$num2 | tonumber].id | tonumber' $txbtool/$2.json`
+                          jq --arg var $bucksrc/$file1/$file2/$id".mp4" --arg num1 $pos1 --arg num2 $pos2 '.albums[$num1 | tonumber].videos[$num2 | tonumber].src=$var' $txbtool/$2.json > $txbtool/var1.json
+                          mv $txbtool/var1.json $txbtool/$2.json
+                          jq --arg var $buckdst/$file2/$id/$id".m3u8" --arg num1 $pos1 --arg num2 $pos2 '.albums[$num1 | tonumber].videos[$num2 | tonumber].dst=$var' $txbtool/$2.json > $txbtool/var1.json
+                          mv $txbtool/var1.json $txbtool/$2.json
+                          jq --arg var $buckdst/$file2/$id"/00001.jpg" --arg num1 $pos1 --arg num2 $pos2 '.albums[$num1 | tonumber].videos[$num2 | tonumber].thumbnails=$var' $txbtool/$2.json > $txbtool/var1.json
+                          mv $txbtool/var1.json $txbtool/$2.json
+                          jq --arg var $buckdst/$file2/$id".png" --arg num1 $pos1 --arg num2 $pos2 '.albums[$num1 | tonumber].videos[$num2 | tonumber].pagepic=$var' $txbtool/$2.json > $txbtool/var1.json
+                          mv $txbtool/var1.json $txbtool/$2.json
 
-                          if [ -f $1"/"$file1"/"$file2"/"$id".png" ]; then
-                            cp -rf $1"/"$file1"/"$file2"/"$id".png" $buckdst
+                          if [ -f $1/$file1/$file2/$id".png" ]; then
+                            cp -rf $1/$file1/$file2/$id".png" $buckdst
                           fi
                           pos2=`expr $pos2 + 1`
                   done
@@ -84,7 +84,7 @@ function fun(){
                 done
         fi
         done
-        cat $txbtool"/"$2.json
+        cat $txbtool/$2.json
 }
 
 fun $1 $2
