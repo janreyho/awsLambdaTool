01b17f9a3e040af3f6f6eb0d5fde018b5554f058
hejiayi <janreyho@gmail.com>
2016-03-22 11:50:51 +0800

    curl -F stream=@jsonpath

--- startjob.sh
+++ startjob.sh
@@ -5,13 +5,12 @@ echo $var
 for file in ` ls ~/gochina/gochina/cpconfig `
 do
 {
-	ps -fe | grep "$userfolder/gochina/gochina/gochinajob.sh $1 $file" | grep -v grep 
+	ps -fe | grep "$userfolder/gochina/gochina/gochinajob.sh $1 $file" | grep -v grep
 	if [ $? -ne 0 ]; then
 	~/gochina/gochina/gochinajob.sh $1 $file >> ~/gochina/$file/log/$var 2>&1
 	/usr/bin/mail -s $file'-'$var'更新' janreyho@gmail.com < ~/gochina/$file/log/$var
 	else
-		echo "$userfolder/gochina/gochina/gochinajob.sh $1 $file exist"
+		echo "$userfolder/gochina/gochina/gochinajob.sh $1 $file exist" >> ~/gochina/$file/log/$var
 	fi
 } &
 done
-
