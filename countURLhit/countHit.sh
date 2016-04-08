3c2680cf0297a14f1d30c786fcff096c6a7c7e82
hejiayi <janreyho@gmail.com>
2016-04-08 18:59:06 +0800

    ./countHit.sh http://xxx.com url.txt

---
+++ countURLhit/countHit.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# ./countHit.sh http://xxx.com url.txt
+
+url="null"
+linenum=0
+urlnum=0
+cacheNum=0
+hitnum=0
+echo "" > awkalldata.txt
+while true
+do
+  linenum=`expr $linenum + 1`
+  echo $linenum
+  echo $linenum >> awkalldata.txt
+  url=`sed -n $linenum"p" $2`
+  if [[ "" == $url ]];then
+  	break
+  fi
+  # `(( $linenum++ ))`
+
+  if [[ "null" != $url ]];then
+  	urlnum=`expr $urlnum + 1`
+  	echo $1$url
+  	echo $1$url >> awkalldata.txt
+  	curl -I $1$url > /tmp/awkdata.txt
+  	cat /tmp/awkdata.txt >> awkalldata.txt
+  	hitstr=`awk 'BEGIN{FS=": "} /X-Cache/{print $2}' /tmp/awkdata.txt`
+  	echo $hitstr
+  	if [[ "" != $hitstr ]];then
+  		cacheNum=`expr $cacheNum + 1`
+  	fi
+  	# if [[ "HIT" == $hitstr ]];then
+  	if [[ $hitstr =~ "HIT" ]]; then
+  		hitnum=`expr $hitnum + 1`
+  	fi
+  fi
+done
+
+echo "linenum:"$linenum
+echo "urlnum:"$urlnum
+echo "cacheNum:"$cacheNum
+echo "hitnum:"$hitnum
