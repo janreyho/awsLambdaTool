b853824973a07d39ef7aa714dd882834c4acc277
hejiayi <janreyho@gmail.com>
2016-05-23 17:49:18 +0800

    -f /gochina//log/resolution_

--- startjob.sh
+++ startjob.sh
@@ -9,7 +9,11 @@ do
 	if [ $? -ne 0 ]; then
 		# if [ "no" = $1 ];then
 			/gochina/gochina/gochinajob.sh $1 $file $var >> /gochina/$file/log/$var 2>&1
+			if [ -f /gochina/$file/log/resolution_$var ]; then
 			/usr/bin/mail -s $file'-'$var'更新' $mailrecverstest < /gochina/$file/log/$var
+			else
+				rm /gochina/$file/log/$var
+			fi
 		# else
 		# 	/gochina/gochina/gochinajob.sh $1 $file $var
 		# fi
