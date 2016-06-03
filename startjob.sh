90febeb4fbdfa5421a7081f1ea1c91157323a734
hejiayi <janreyho@gmail.com>
2016-06-03 15:45:32 +0800

    解决日志冗余和发送问题

--- startjob.sh
+++ startjob.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+ps -fe | grep "/gochina/gochina/gochinajob.sh $1" | grep -v grep
+if [ $? -eq 0 ]; then
+	exit 0
+fi
+
 var=` TZ='Asia/Shanghai' date +%Y%m%d%H%M%S `
 echo $var
 
@@ -5,20 +11,12 @@ for file in ` ls /gochina/gochina/cpconfig `
 do
 {
 	source /gochina/gochina/cpconfig/$file
-	ps -fe | grep "/gochina/gochina/gochinajob.sh $1 $file" | grep -v grep
-	if [ $? -ne 0 ]; then
-		# if [ "no" = $1 ];then
 			/gochina/gochina/gochinajob.sh $1 $file $var >> /gochina/$file/log/$var 2>&1
-			if [ -f /gochina/$file/log/resolution_$var ]; then
+			echo "/gochina/$file/log/resolution_$var.txt"
+			if [ -f /gochina/$file/log/resolution_$var".txt" ]; then
 			/usr/bin/mail -s $file'-'$var'更新' $mailrecverstest < /gochina/$file/log/$var
 			else
 				rm /gochina/$file/log/$var
 			fi
-		# else
-		# 	/gochina/gochina/gochinajob.sh $1 $file $var
-		# fi
-	# else
-	# 	echo "/gochina/gochina/gochinajob.sh $1 $file exist" >> /gochina/$file/log/$var
-	fi
 } &
 done
