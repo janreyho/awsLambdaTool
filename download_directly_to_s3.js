b72ffb03b433daa0733105d95fd038e681d116ea
Andy Wang <wangyaodi@gmail.com>
2016-06-29 18:30:53 +0800

    refactor download script to include pusher and push notification

--- download_directly_to_s3.js
+++ download_directly_to_s3.js
@@ -2,6 +2,9 @@
 npm install
 export AWS_ACCESS_KEY_ID=<key>
 export AWS_SECRET_ACCESS_KEY=<secret>
+export PUSHER_APP_ID=<app_id>
+export PUSHER_KEY=<key>
+export PUSHER_SECRET=<secret>
 node download_directly_to_s3.js
 */
 var streamingS3 = require('streaming-s3');
@@ -10,9 +13,18 @@ var Consumer = require('sqs-consumer');
 var argv = require('minimist')(process.argv.slice(2));
 var http = require('http');
 var querystring = require('querystring');
+var uploadedBytes = 0;
+var Pusher = require('pusher');
 // var airbrake = require('airbrake').createClient("117109", "3d3cc8749439b5878038d22d079ce0bf");
 
-function upload(url, out) {
+var pusher = new Pusher({
+  appId: '220674',
+  key: '65a7daa2d92d664e54ec',
+  secret: '7435d50f2981e18de1d5',
+  encrypted: true
+});
+
+function upload(url, out, size, eid) {
   var bucket = 'ottcloud';
 
   var rStream = request.get(url);
@@ -31,7 +43,16 @@ function upload(url, out) {
   );
 
   uploader.on('data', function (bytesRead) {
+    uploadedBytes = uploadedBytes + bytesRead
     console.log(bytesRead, ' bytes read.');
+    console.log("uploadedBytes:", uploadedBytes);
+    console.log("Uploading Rate:", uploadedBytes/size);
+    if(size > 0) {
+      if(uploadedBytes/size < 1 && uploadedBytes/size*100%10 < 0.3) {
+        notify_download_progress(eid, uploadedBytes/size);
+        pusher.trigger('vego_channel', 'get_download_progress_'+eid, {'rate': uploadedBytes/size});
+      }
+    }
   });
 
   uploader.on('part', function (number) {
@@ -45,21 +66,46 @@ function upload(url, out) {
 
   uploader.on('finished', function (resp, stats) {
     console.log('Upload finished: ', resp);
-    notify_to_boss(resp.Location, stats);
+    console.log('State:', stats);
+    uploadedBytes = 0;
+    notify_to_boss(eid, stats);
+    pusher.trigger('vego_channel', 'finish_'+eid, {'rate': 1});
   });
 
   uploader.on('error', function (e) {
     console.log('Upload error: ', e);
+    notify_download_progress(eid, '-1');
+    pusher.trigger('vego_channel', 'get_error_'+eid, {'rate': -1});
   });
 
   uploader.begin(); // important if callback not provided.
 }
 
-function notify_to_boss(url, stats){
-  var post_data = querystring.stringify({'url' : url, 'state' : stats });
+function notify_to_boss(eid, stats){
+  var post_data = querystring.stringify({'eid' : eid, 'state' : stats });
   var req = http.request({
-    host: 'vrsclone.herokuapp.com',
+    // host: 'vrsclone.herokuapp.com',
+    host: 'mgtv.admin.vrsclone.dev',
     path: '/api/v1/episodes/transfer.json',
+    method: 'POST',
+    headers: {
+      'X-REQUESTER': 'BOSS-DOWNLOAD',
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': Buffer.byteLength(post_data)
+    }
+  }, function(res) {
+    res.setEncoding('utf8');
+  });
+  req.write(post_data);
+  req.end();
+}
+
+function notify_download_progress(eid, rate){
+  var post_data = querystring.stringify({'eid' : eid, 'rate' : rate });
+  var req = http.request({
+    // host: 'vrsclone.herokuapp.com',
+    host: 'mgtv.admin.vrsclone.dev',
+    path: '/api/v1/episodes/download_progress.json',
     method: 'POST',
     headers: {
       'X-REQUESTER': 'BOSS-DOWNLOAD',
@@ -80,11 +126,11 @@ function run_queue_server() {
     queueUrl: 'https://sqs.us-east-1.amazonaws.com/629005883304/download_' + env,
     handleMessage: function (message, done) {
       // do some work with `message`
-      //console.log(message.Body);
+      console.log(message.Body);
       try {
         var task = JSON.parse(message.Body);
         console.log(task);
-        upload(task['url'], task['out']);
+        upload(task['url'], task['out'], task['size'], task['eid']);
 
       } catch (e) {
         console.log(e);
@@ -108,8 +154,10 @@ if (require.main === module) {
     console.log("running example");
     var url = "https://s3.amazonaws.com/ottcloud-video/zgl/rm.zip";
     var out = "zgl/rm-1.zip";
+    var size = 1232242342;
+    var eid = 8332
     try {
-      upload(url, out);
+      upload(url, out, size, eid);
     } catch (e) {
       console.log(e);
       // airbrake.notify(e);
