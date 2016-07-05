2dfc5c99d9beacd8fd8e904f74a226fd85b07984
hejiayi <janreyho@gmail.com>
2016-07-05 17:32:59 +0800

    txb ggjk change to use ses send email

--- sendmail.py
+++ sendmail.py
@@ -10,11 +10,11 @@ import boto.ses
 from boto.s3.connection import S3Connection
 
 def usage():
-  print "use:python sendmail.py -s subject -b body"
-  sys.exit()
+    print "use:python sendmail.py -s subject -f file"
+    sys.exit()
 
 def procpara(para):
-    opts, args = getopt.getopt(sys.argv[1:], "hs:b:")
+    opts, args = getopt.getopt(sys.argv[1:], "hs:f:")
     print sys.argv
     num = len(sys.argv)
     if 5 != num:
@@ -23,8 +23,8 @@ def procpara(para):
         sys.exit()
 
     for op, value in opts:
-        if op == "-b":
-            para['-b'] = value
+        if op == "-f":
+            para['-f'] = value
         elif op == "-s":
             para['-s'] = value
         elif op == "-h":
@@ -34,8 +34,7 @@ def sendemail(subject,data):
     sesconn = boto.ses.connect_to_region('us-west-2')
 
     email_from = 'janreyho@gmail.com'
-    email_to = ['hejiayi@gochinatv.com','zhixueyong@gochinatv.com','lisi@gochinatv.com','panxinming@gochinatv.com','zhangyue@gochinatv.com','caolei@gochinatv.com']
-
+    email_to = ['hejiayi@gochinatv.com','zhixueyong@gochinatv.com','lisi@gochinatv.com','panxinming@gochinatv.com','zhangyue@gochinatv.com','caolei@gochinatv.com','yangyang@gochinatv.com','yuzebin@gochinatv.com','fengzhiwen@gochinatv.com']
     response = sesconn.send_email(email_from,subject,data,email_to)
 
     print response
@@ -45,7 +44,12 @@ if __name__ == '__main__':
     procpara(para)
 
     print para['-s']
-    print para['-b']
+    print para['-f']
 
-    sendemail(para['-s'],para['-b'])
+    # fp=commands.getoutput("echo "+ para['-f'])
+    # print fp
+    f=open(para['-f'])
+    ff=f.read()
+    print ff
+    # sendemail(para['-s'],ff)
 
--- startjob.sh
+++ startjob.sh
@@ -14,7 +14,8 @@ do
 			/gochina/gochina/gochinajob.sh $1 $file $var >> /gochina/$file/log/$var 2>&1
 			echo "/gochina/$file/log/resolution_$var.txt"
 			if [ -f /gochina/$file/log/resolution_$var".txt" ]; then
-			/usr/bin/mail -s $file'-'$var'更新' $mailrecverstest < /gochina/$file/log/$var
+			python $txbtool"/"sendmail.py -s $file'-'$var'更新' -f /gochina/$file/log/$var
+			# /usr/bin/mail -s $file'-'$var'更新' $mailrecverstest < /gochina/$file/log/$var
 			else
 				rm /gochina/$file/log/$var
 			fi
